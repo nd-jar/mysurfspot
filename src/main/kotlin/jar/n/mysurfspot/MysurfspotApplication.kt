97b729a038c2963aa6a2dbd79a950e003cce8e81
NJar <nzharkov@cinimex.ru>
2020-03-27 23:42:04 +0300

    Added properties

--- src/main/kotlin/jar/n/mysurfspot/MysurfspotApplication.kt
+++ src/main/kotlin/jar/n/mysurfspot/MysurfspotApplication.kt
@@ -3,11 +3,13 @@ package jar.n.mysurfspot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.PropertySource
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
+@PropertySource("file:src/main/resources/application.properties")
 class MysurfspotApplication
 
 fun main(args: Array<String>) {
